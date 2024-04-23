cf9d13acea0b6ecb3312f07c406520d4fc82289f
Nadia <nad21lc@gmail.com>
2024-04-22 21:56:30 -0500

    feat: Added all models

---
+++ models.py
@@ -0,0 +1,103 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Date, DateTime, Float
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class City(Base):
+    __tablename__ = "city"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+
+
+class District(Base):
+    __tablename__ = "district"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    cityId = Column(Integer, ForeignKey('city.id'), nullable=False)
+
+
+class Ubication(Base):
+    __tablename__ = "ubication"
+
+    id = Column(Integer, primary_key=True, index=True)
+    address = Column(String(200), nullable=False)
+    districtId = Column(Integer, ForeignKey('district.id'), nullable=False)
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    password = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+
+class Persona(Base):
+    __tablename__ = "persona"
+
+    id = Column(Integer, primary_key=True, index=True)
+    firstName = Column(String(50), nullable=False)
+    lastName = Column(String(50), nullable=False)
+    avatar = Column(String(500), nullable=False)
+    role = Column(Boolean, nullable=False)
+    phone = Column(String(15), nullable=False)
+    birthdate = Column(Date, nullable=False)
+    money = Column(Float(10, 2), nullable=False)
+    description = Column(String(50), nullable=False)
+    rating = Column(Integer, nullable=False)
+    userId = Column(Integer, ForeignKey('user.id'), nullable=False)
+    ubicationId = Column(Integer, ForeignKey('ubication.id'), nullable=False)
+
+class Profession(Base):
+    __tablename__ = "profession"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+
+class Specialist(Base):
+    __tablename__ = "specialist"
+
+    id = Column(Integer, primary_key=True, index=True)
+    workExperience = Column(Float(5, 2), nullable=False)
+    Profession_idProfession = Column(Integer, ForeignKey('profession.id'), nullable=False)
+    personaId = Column(Integer, ForeignKey('persona.id'), nullable=False)
+
+class Publication(Base):
+    __tablename__ = "publication"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50), nullable=False)
+    description = Column(String(200), nullable=False)
+    address = Column(String(200), nullable=False)
+    image = Column(String(500), nullable=False)
+    is_publish = Column(Boolean, nullable=False)
+    personaId = Column(Integer, ForeignKey('persona.id'), nullable=False)
+    
+class Contract(Base):
+    __tablename__ = "contract"
+
+    id = Column(Integer, primary_key=True, index=True)
+    price = Column(Float(10, 2), nullable=False)
+    state = Column(Integer, nullable=False)
+    appointmentDate = Column(DateTime, nullable=False)
+    publicationId = Column(Integer, ForeignKey('publication.id'), nullable=False)
+    specialistId = Column(Integer, ForeignKey('specialist.id'), nullable=False)
+    
+class Review(Base):
+    __tablename__ = "review"
+
+    id = Column(Integer, primary_key=True, index=True)
+    contractId = Column(Integer, ForeignKey('contract.id'), nullable=False)
+    comment = Column(String(100), nullable=False)
+    rating = Column(Integer, nullable=False)
+    
+class ClientReview(Base):
+    __tablename__ = "clientReview"
+    personaId = Column(Integer, ForeignKey('persona.id'), primary_key=True, index=True)
+    reviewId = Column(Integer, ForeignKey('review.id'), primary_key=True, index=True)
+
+class SpecialistReview(Base):
+    __tablename__ = "specialistReview"
+    reviewId = Column(Integer, ForeignKey('review.id'), primary_key=True, index=True)
+    specialistId = Column(Integer, ForeignKey('specialist.id'), primary_key=True, index=True)
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# API de AlToque hecha con FastAPI
+
+# Guía de Instalación
+
+- pip install virtualenv
+- virtualenv venv
+- .\venv\Scripts\activate.ps11
+- Hago el requirements.txt
+- pip install -r .\requirements.txt
+- uvicorn main:app --port 8001
