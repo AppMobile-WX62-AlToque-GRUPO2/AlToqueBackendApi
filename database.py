5a80583ff0a39e1572a45198ac1a5f26b26c9698
Nadia Lucas <91857416+nad21lc@users.noreply.github.com>
2024-04-29 09:53:06 -0500

    Update database.py

--- database.py
+++ database.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-DB_HOST = "roundhouse.proxy.rlwy.net"
-DB_USER = "root"
-DB_PASSWORD = "GqxRyzzDxMgRCAsadwNhDefsnSQhkhvF"
-DB_NAME = "railway"
-DB_PORT = "57433"
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'root'
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = '3306'
+MYSQL_DATABASE = 'altoquedb'
 
-URL_DATABASE = f'mysql+pymysql://{DB_HOST}:{DB_USER}@{DB_PASSWORD}:{DB_PORT}/{DB_NAME}'
+URL_DATABASE = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
 
 engine = create_engine(URL_DATABASE)
 
