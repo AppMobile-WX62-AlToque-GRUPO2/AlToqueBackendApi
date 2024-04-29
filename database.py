d03109c8b12558dfb568335c3d0e7f2cca69bda6
Nadia Lucas <91857416+nad21lc@users.noreply.github.com>
2024-04-29 08:36:07 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'root'
-MYSQL_HOST = 'localhost'
-MYSQL_PORT = '3306'
-MYSQL_DATABASE = 'altoquedb'
+DB_HOST = os.environ.get("DB_HOST")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+DB_PORT = os.environ.get("DB_PORT")
 
-URL_DATABASE = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
+URL_DATABASE = f'mysql+pymysql://{DB_HOST}:{DB_USER}@{DB_PASSWORD}:{DB_PORT}/{DB_NAME}'
 
 engine = create_engine(URL_DATABASE)
 
