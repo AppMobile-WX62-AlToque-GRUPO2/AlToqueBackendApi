a8a77933ba20353ddfaecffb8282db765f196e8a
Nadia <nad21lc@gmail.com>
2024-05-24 19:05:28 -0500

    fix: Added configuration in users routes

--- routes/routes.py
+++ routes/routes.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 from config.database import get_db
 import models.models as models
-from schemas.schemas import UserBase, NotificationBase, ContractBase, PostBase, AvailableDateBase, ClientBase, ProfessionBase, SpecialistBase, ProvinceBase, CityBase, DistrictBase, UbicationBase, ReviewBase, Review
+from schemas.schemas import UserUpdate, UserBase, NotificationBase, ContractBase, PostBase, AvailableDateBase, ClientBase, ProfessionBase, SpecialistBase, ProvinceBase, CityBase, DistrictBase, UbicationBase, ReviewBase, Review
 
 from routes.auth import hash_password
 
@@ -35,7 +35,7 @@ async def create_user(user: UserBase, db: db_dependency):
     db.commit()
 
 @router.put("/users/{user_id}", status_code=status.HTTP_200_OK, tags=["Users"])
-async def update_user(user_id: int, user: UserBase, db: db_dependency):
+async def update_user(user_id: int, user: UserUpdate, db: db_dependency):
     db_user = db.query(models.User).filter(models.User.id == user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
