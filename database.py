13509803266140a5ee49ba0e8cb6b142a3c5ee8f
OnofreR <carlosonofreruiz16@gmail.com>
2024-06-12 22:42:20 -0500

    database.py

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'serpiente456*'
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = '3306'
+MYSQL_DATABASE = 'altoquedb'
+
+URL_DATABASE = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
